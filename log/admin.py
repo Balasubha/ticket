d1dc805c0454844a5a58223947629accac6a46a6
Ayns <ayyanar.m@gmail.com>
2019-09-10 10:12:15 +0530

    Models added

--- log/admin.py
+++ log/admin.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from log.models import UserProfileInfo, User
+from log.models import UserProfileInfo, User, department, ticket
 
 # Register your models here.
 admin.site.register(UserProfileInfo)
+admin.site.register(department)
+admin.site.register(ticket)
--- log/models.py
+++ log/models.py
@@ -14,3 +14,26 @@ class UserProfileInfo(models.Model):
 def __str__(self):
     return self.user.username
     
+class department (models.Model):
+    dept = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.dept
+
+class ticket (models.Model):
+    Date = models.DateField()
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    Dept = models.ForeignKey(department, on_delete=models.CASCADE)
+    Subject = models.CharField(max_length=60)
+    Details = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.Subject
+
+
+class users (models.Model):
+    UserName = models.CharField(max_length=20, null=False,unique=True)
+    Name= models.CharField(max_length=20)
+    Email = models.EmailField(max_length=70, blank=True)
+    dept = models.ForeignKey(department, on_delete=models.CASCADE)
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
